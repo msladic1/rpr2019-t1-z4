225c3ec87da05b3b00b3106858bf28a75655e041
msladic1 <msladic1@etf.unsa.ba>
2020-10-26 19:12:05 +0100

    U klasu Supermarket dodana metoda izbaciArtiklSaKodom

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -12,6 +12,21 @@ public class Supermarket {
     }
 
     public Artikl[] getArtikli() {
-        return artikli;
+        return this.artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String s) {
+        for(int i = 0; i < this.brojArtikala; i++){
+            if(this.artikli[i].getKod().equals(s)){
+                Artikl artikl = new Artikl(this.artikli[i]);
+                brojArtikala = brojArtikala - 1;
+                for(int j = i; j < this.brojArtikala; j++) {
+                    this.artikli[j] = this.artikli[j+1];
+                }
+                this.artikli[brojArtikala] = null;
+                return artikl;
+            }
+        }
+        return null;
     }
 }
