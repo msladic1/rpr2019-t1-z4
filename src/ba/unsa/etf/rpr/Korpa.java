027e4329c6af9a4161708757dea173edf9333b70
msladic1 <msladic1@etf.unsa.ba>
2020-10-26 18:53:07 +0100

    U klasu Korpa dodan geter

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,10 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    private Artikl[] artikli = new Artikl[50];
+    private int brojArtikala = 0;
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
 }
