856e323f036df87895ec4b04a3eda078ed060734
msladic1 <msladic1@etf.unsa.ba>
2020-10-26 18:50:26 +0100

    Dodani konstruktori klase Artikl

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,22 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    private String naziv;
+    private int cijena;
+    private String kod;
+
+    public Artikl(String naziv, int cijena, String kod) {
+        this.naziv = naziv;
+        this.cijena = cijena;
+        this.kod = kod;
+    }
+
+    public Artikl(Artikl artikl) {
+        naziv = artikl.naziv;
+        kod = artikl.kod;
+        cijena = artikl.cijena;
+    }
+
+
+
 }
