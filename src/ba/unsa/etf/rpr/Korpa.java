614f0f6353489de26bb02ce02fc0698c6a7ce205
msladic1 <msladic1@etf.unsa.ba>
2020-10-26 19:16:17 +0100

    U klasu Korpa dodana metoda izbaciArtiklSaKodom

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -17,4 +17,19 @@ public class Korpa {
         else
             return false;
     }
+
+    public Artikl izbaciArtiklSaKodom(String s) {
+        for(int i = 0; i < this.brojArtikala; i++){
+            if(this.artikli[i].getKod().equals(s)){
+                Artikl artikl = new Artikl(this.artikli[i]);
+                brojArtikala = brojArtikala - 1;
+                for(int j = i; j < this.brojArtikala; j++) {
+                    this.artikli[j] = this.artikli[j+1];
+                }
+                this.artikli[brojArtikala] = null;
+                return artikl;
+            }
+        }
+        return null;
+    }
 }
