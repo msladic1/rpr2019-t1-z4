2303bdc608cb32372e2374c31ec2a5fc08ab1a84
msladic1 <msladic1@etf.unsa.ba>
2020-10-26 18:54:40 +0100

    U klasu Supermarket dodana metoda dodajArtikl

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,5 +1,13 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] artikl = new Artikl[1000];
+    private int brojArtikala = 0;
 
+    public void dodajArtikl(Artikl artikl) {
+        if(brojArtikala < 1000) {
+            this.artikl[brojArtikala] = new Artikl(artikl);
+            brojArtikala = brojArtikala + 1;
+        }
+    }
 }
