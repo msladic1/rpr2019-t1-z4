016c8f892617d774ad837ad622cd952bb0191c29
msladic1 <msladic1@etf.unsa.ba>
2020-10-26 18:57:35 +0100

    U klasu Supermarket dodan geter

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,13 +1,17 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
-    private Artikl[] artikl = new Artikl[1000];
+    private Artikl[] artikli = new Artikl[1000];
     private int brojArtikala = 0;
 
     public void dodajArtikl(Artikl artikl) {
         if(brojArtikala < 1000) {
-            this.artikl[brojArtikala] = new Artikl(artikl);
+            this.artikli[brojArtikala] = new Artikl(artikl);
             brojArtikala = brojArtikala + 1;
         }
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
     }
 }
